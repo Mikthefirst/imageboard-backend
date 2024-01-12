84cbdb1a905892fe8fa08b197a78215b9e508a2f
Mikhail <mikthefirst11@gmail.com>
2024-01-12 12:19:57 +0300

    The basics of future app

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
+app.use(express.json());
+app.use(cors());
 
 app.get('/api', (req, res) => {
     res.json({
@@ -8,6 +11,9 @@ app.get('/api', (req, res) => {
     });
 })
 
+app.post('/api', (req, res) => {
+    console.log(req.body);
+})
 app.listen(port, () => {
     console.log('start listening on port ', port);
 });
